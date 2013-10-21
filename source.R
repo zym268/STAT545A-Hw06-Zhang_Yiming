9007a43664b2d53f65f1d809b538d2505a8e9924
zym268 <yiming0630@hotmail.com>
2013-10-21 02:53:37 -0700

    Update source.R

--- source.R
+++ source.R
@@ -1,3 +1,11 @@
+## clean out any previous work
+outputs <- c("gDat_cl.tsv", 
+             "ests.tsv.tsv", 
+             "Best_Worest_countries_each_continents_estimation.tsv"
+             list.files(pattern = "*.png$"))
+file.remove(outputs)
+
+
 ## run scripts
 source("Hw06 script01.R")
 source("Hw06 script02.R")
