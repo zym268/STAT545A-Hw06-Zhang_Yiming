25ff3e4478f2c9fff2f3bfd4335e0ef41a2803d5
zym268 <yiming0630@hotmail.com>
2013-10-30 15:22:04 -0700

    tidy up the code

--- Hw06 script01.R
+++ Hw06 script01.R
@@ -12,30 +12,32 @@ library(ggplot2)
 str(gDat) 
 
 ##get the number of unique country in every continent in gDat 
-(countcountry <- ddply(gDat,~continent,summarize,count_country=length(unique(country))))
+(countcountry <- ddply(gDat, ~ continent, summarize, count_country = length( unique( country))))
 
 ##drop Oceania since it has only 2 contries
-gDat <- droplevels(subset(gDat,continent !="Oceania")) 
+gDat <- droplevels( subset( gDat, continent !="Oceania")) 
 
 ##check out the general picture of life expectancy in these four continents 
 stripplot(lifeExp ~ continent, gDat, jitter.data = TRUE, grid = "h", 
           type = c("p", "a"), fun = median) 
 
 #plot the life expectancy varing trend for the four continents
-LifeExpchgebyCont_tall <- ddply(gDat, ~year + continent, summarize, MedianLifeExp = median(lifeExp))
-ggplot(LifeExpchgebyCont_tall,aes(x=year,y=MedianLifeExp,colour=continent))+geom_point()+geom_line()+stat_smooth(method = "lm")+facet_wrap(~continent)+ggtitle("Life expectancy by median in four continents")
+LifeExpchgebyCont_tall <- ddply( gDat, ~ year + continent, summarize, MedianLifeExp = median(lifeExp))
+ggplot(LifeExpchgebyCont_tall, aes( x = year, y = MedianLifeExp,
+                                  colour = continent)) + geom_point() + geom_line() + stat_smooth(method = "lm") + facet_wrap(~ continent) + ggtitle("Life expectancy by median in four continents")
 ggsave("lifeExp_in_continent_by_time_4polts.png")
-ggplot(LifeExpchgebyCont_tall,aes(x=year,y=MedianLifeExp,colour=continent))+geom_point()+geom_line()+ggtitle("Life expectancy by median in four continents")
+ggplot(LifeExpchgebyCont_tall, aes( x = year, y = MedianLifeExp, 
+                                  colour = continent)) + geom_point() + geom_line() + ggtitle("Life expectancy by median in four continents")
 ggsave("lifeExp_in_continent_by_time_1polts.png")
 
 ##plot and print the density plot on gdpPercap for four coninents in 2007
 select_year <- "2007" # avoid the magic number
-gDat_select_year <- subset(gDat,subset=year==select_year)
-ggplot(gDat_select_year,aes(x=gdpPercap,colour=continent))+geom_density()
+gDat_select_year <- subset( gDat, subset = year == select_year)
+ggplot(gDat_select_year, aes(x = gdpPercap, colour = continent)) + geom_density()
 ggsave("gdpPercap_in_continent_densitypolts.png")
 
 ##plot the density plot on lifeExp for four coninents in 2007
-ggplot(gDat_select_year,aes(x=lifeExp,colour=continent))+geom_density()
+ggplot(gDat_select_year, aes( x = lifeExp, colour = continent)) + geom_density()
 ggsave("lifeExp_in_continent_densitypolts.png")
 
 ## plot with gdpPercap as x asis, lifeExp as y asis and square root of population as size of each plot
